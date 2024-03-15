4fb9c1f227904e45e62f0e5886da8127a5c235e4
Ulvi Spaethe <ulvispaethe@gmail.com>
2024-03-15 19:05:58 +0000

    styling bug fixed

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -44,7 +44,7 @@ const ProductDetails = () => {
   };
 
   return (
-    <section className="pt-32 pb-12 lg:py-32 h-screen flex items-center">
+    <section className="pt-32 pb-12 lg:py-32 min-h-[800px] flex items-center">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row items-center">
           <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0">
